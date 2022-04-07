b1cbbff9d20abbd7c226eb2f869da1e018e4ef86
Ben Kuskopf <kussie@gmail.com>
2022-04-07 19:38:26 +1000

    Nova 4.0 Support

--- src/NovaActivitylog.php
+++ src/NovaActivitylog.php
@@ -27,6 +27,7 @@ class NovaActivitylog extends Tool
      * Build the menu that renders the navigation links for the tool.
      *
      * @param  \Illuminate\Http\Request $request
+     *
      * @return mixed
      */
     public function menu(Request $request)
